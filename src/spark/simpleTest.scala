fb708af4add0f93b685c7298564c76e1b1b21d60
Junlang He <jhe@matlo.com>
2017-08-04 17:53:41 +0200

    nth

---
+++ src/spark/simpleTest.scala
@@ -0,0 +1,12 @@
+/**
+ *
+ * Created by junlang on 8/4/17.
+ */
+
+package spark
+
+import org.apache.spark.ml.clustering.KMeansForMixedData
+
+object simpleTest {
+  val kmfdm = new KMeansForMixedData()
+}
